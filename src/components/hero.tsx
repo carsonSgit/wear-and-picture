2a536a02182b6e3a37717edad3aaa0fe3323b0f6
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-10-28 00:19:18 -0400

    feat: Hover text scrambler

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,21 +1,36 @@
-'use client'
+'use client';
 
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import { useTheme, useMediaQuery } from '@mui/material';
 import styles from "./hero.module.css";
+import { initTextScramble } from '@/scripts/scrambletext';
 
 const Hero: React.FC = () => {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const isMediumScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
-
     const fontSize = isSmallScreen ? '2rem' : isMediumScreen ? '3rem' : '4rem';
+
+    const textSegments = ["wear", "%20", "&", "%20", "picture", "."];
 
     return (
         <div className={styles.hero}>
-            <Typography variant="h1" style={{ fontSize, fontFamily: 'Canva Sans, sans-serif', fontWeight:600 }}>
-                wear<span className={styles.special}>%20</span>&<span className={styles.special}>%20</span>picture<span className={styles.special}>.</span>
+            <Typography variant="h1" style={{ fontSize, fontFamily: 'JetBrains Mono, monospace', fontWeight: 600 }}>
+                {textSegments.map((segment, index) => (
+                    <span
+                        key={index}
+                        className={segment === "%20" || segment === "." ? styles.special : styles.segment}
+                        data-value={segment}
+                        ref={(ref) => {
+                            if (ref) {
+                                initTextScramble(ref);
+                            }
+                        }}
+                    >
+                        {segment}
+                    </span>
+                ))}
             </Typography>
         </div>
     );
