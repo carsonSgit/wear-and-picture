07e3ead3a333923a53ea1d77bd88af0e4673d18d
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-10-31 12:09:01 -0400

    chore: Initial grid layout done

--- src/app/[blog]/blogTemplate.tsx
+++ src/app/[blog]/blogTemplate.tsx
@@ -4,11 +4,11 @@ export default function BlogTemplate(){
     return(
         <div className={styles.grid}>
             <div className={styles.left}>
-                <img className={styles.leftImage} src="blog.png" />
-                <h1>I am on the left</h1>
+                <img className={styles.blogImage} src="blog.png" />
             </div>
             <div className={styles.right}>
-                <h1>I am on the right</h1>
+                <h1 className={styles.blogTitle}>I am on the right</h1>
+                <p className={styles.blogText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
         </div>
     )
