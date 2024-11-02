70e27d1915c678f13ec2381da731cb713fd19b69
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-11-01 23:48:23 -0400

    chore: Start pics page

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -1,7 +1,12 @@
-export default function Blog(){
+import ScrollingText from "@/components/ScrollingText";
+import styles from "./pics.module.css";
+
+export default function Pics(){
     return (
-        <div>
-          <h1>Test</h1>
+        <div className={styles.container}>
+          <div className={styles.picsContainer}>
+            <img className={styles.pics} src="pics_1.webp" />
+          </div>
         </div>
     );
 }
