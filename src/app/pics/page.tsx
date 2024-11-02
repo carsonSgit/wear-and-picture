b82f40c735f58d9546ceac804d16a1eba7ada4db
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-11-02 00:37:10 -0400

    feat: Pics page

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -1,5 +1,7 @@
-import ScrollingText from "@/components/ScrollingText";
+'use client';
+
 import styles from "./pics.module.css";
+import { Typewriter } from 'react-simple-typewriter';
 
 export default function Pics(){
     return (
@@ -7,6 +9,21 @@ export default function Pics(){
           <div className={styles.picsContainer}>
             <img className={styles.pics} src="pics_1.webp" />
           </div>
+            <p className={styles.typewriter}>
+              <Typewriter 
+              words={[
+                'a picture is worth a thousand words.',
+                '1000 words = 1 picture.',
+                'a picture is worth 1000 words.'
+              ]}
+              loop={true}
+              cursor
+              cursorStyle=">>"
+              typeSpeed={100}
+              deleteSpeed={50}
+              delaySpeed={8000}
+            />
+          </p>
         </div>
     );
 }
