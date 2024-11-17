05078d3e6018e514ceecfc260ab56880f51bfd34
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-11-16 21:46:26 -0500

    polish: Favicon + gen. polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "WP",
-  description: "Wear & Picture",
+  description: "Wear & Picture"
 };
 
 export default function RootLayout({
@@ -26,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable}`}>
+      <head>
+        <link rel="icon" href=".test.ico" />
+      </head>
+      <body>
         <Navbar />
         {children}
       </body>
