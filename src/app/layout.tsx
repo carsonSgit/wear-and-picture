f8037f6ade6dfb709ca42e173c01cd02c969e80d
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-12-15 14:22:12 -0500

    feat: Navbar font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
 export const metadata: Metadata = {
   title: "WP",
-  description: "Wear & Picture"
+  description: "Wear & Picture",
 };
 
 export default function RootLayout({
@@ -14,10 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href=".test.ico" />
-      </head>
-      <body>
+      <body className={`${geistSans.variable}`}>
         <Navbar />
         {children}
       </body>
